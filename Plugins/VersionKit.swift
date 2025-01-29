d34e24fc1af78044ce977e4f01652333a71140b4
Nate Smith <nathan.l.smith89@gmail.com>
2025-01-29 19:11:13 +1000

    Lint fix

--- Plugins/VersionKit.swift
+++ Plugins/VersionKit.swift
@@ -2,12 +2,14 @@ import PackagePlugin
 
 @main
 struct VersionKit: BuildToolPlugin {
-    /// Entry point for creating build commands for targets in Swift packages.
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let executable = try context.tool(named: "git").path
+        let arguments = ["describe", "--tags", "--abbrev=0"]
+
         let toolCommand = Command.prebuildCommand(
             displayName: "Git describe",
-            executable: try context.tool(named: "git").path,
-            arguments: ["describe", "--tags", "--abbrev=0"],
+            executable: executable,
+            arguments: arguments,
             outputFilesDirectory: context.pluginWorkDirectory
         )
 
